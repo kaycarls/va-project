4330965cc4b150fc3fb0827a76fb322ce1df826f
Syafiq <syafiq.awangku@hotmail.com>
2023-07-25 21:46:35 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   pages: true,
   runtimeConfig: {
     public: {
-      baseURL: "/va-project/",
+      baseURL: process.env.NUXT_APP_BASE_URL,
     },
   },
   experimental: {
