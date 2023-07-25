87d260278cd59fd939555551944edc1b62a10c46
Syafiq <syafiq.awangku@hotmail.com>
2023-07-25 19:02:59 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,53 @@
 <template>
-  <v-container fluid class="h-screen bg-black text-center">
-    <v-row>
-      <v-col cols="12" class="text-center pt-10">
-        <h1>Hello World</h1>
-        <v-btn class="ma-2 text-none" color="error" @click="handleRecording">
-          {{ recBtn }}
-          <v-icon
-            v-if="!isRecording"
-            end
-            icon="mdi mdi-record-circle-outline"
-          ></v-icon>
-        </v-btn>
-      </v-col>
-      <v-col cols="12" class="text-center pt-10">
-        <div class="d-flex justify-center">
-          <v-textarea
-            v-model="transcript"
+  <ClientOnly>
+    <v-container fluid class="h-screen bg-black text-center">
+      <v-row>
+        <v-col cols="12" class="text-center pt-10">
+          <h1>Hello World</h1>
+          <v-btn class="ma-2 text-none" color="error" @click="handleRecording">
+            {{ recBtn }}
+            <v-icon
+              v-if="!isRecording"
+              end
+              icon="mdi mdi-record-circle-outline"
+            ></v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="12" class="text-center pt-10">
+          <div class="d-flex justify-center">
+            <v-textarea
+              v-model="transcript"
+              variant="outlined"
+              clearable
+              auto-grow
+              class="textarea pa-5"
+            ></v-textarea>
+          </div>
+          <v-btn class="text-none mx-5" color="blue" @click="handleSearch">
+            Search Google
+            <v-icon end icon="mdi mdi-google"></v-icon>
+          </v-btn>
+          <v-btn
+            class="text-none mx-5"
+            color="success"
             variant="outlined"
-            clearable
-            auto-grow
-            class="textarea pa-5"
-          ></v-textarea>
-        </div>
-        <v-btn class="text-none mx-5" color="blue" @click="handleSearch">
-          Search Google
-          <v-icon end icon="mdi mdi-google"></v-icon>
-        </v-btn>
-        <v-btn
-          class="text-none mx-5"
-          color="success"
-          variant="outlined"
-          @click="handleCopy"
-        >
-          Copy Text
-          <v-icon end icon="mdi mdi-content-copy"></v-icon>
-        </v-btn>
-        <v-snackbar
-          v-model="snackbar"
-          class="ma-10"
-          variant="tonal"
-          :color="snackbarColor"
-        >
-          <p class="text-center">{{ snackbarText }}</p>
-        </v-snackbar>
-      </v-col>
-    </v-row>
-  </v-container>
+            @click="handleCopy"
+          >
+            Copy Text
+            <v-icon end icon="mdi mdi-content-copy"></v-icon>
+          </v-btn>
+          <v-snackbar
+            v-model="snackbar"
+            class="ma-10"
+            variant="tonal"
+            :color="snackbarColor"
+          >
+            <p class="text-center">{{ snackbarText }}</p>
+          </v-snackbar>
+        </v-col>
+      </v-row>
+    </v-container>
+  </ClientOnly>
 </template>
 
 <script setup>
@@ -57,35 +59,42 @@ const snackbarText = ref("");
 const snackbarColor = ref("");
 let speech;
 
-onMounted(() => {
+const initSpeechRecognition = () => {
   const Recognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
-  speech = new Recognition();
-  speech.continuous = true;
-  speech.interimResults = true;
-  speech.onstart = () => {
-    snackbarText.value = "Recording Started";
-    snackbarColor.value = "success";
-    snackbar.value = true;
-    isRecording.value = true;
-    recBtn.value = "Stop";
-  };
-  speech.onend = () => {
-    snackbarText.value = "Recording Stopped";
-    snackbarColor.value = "error";
-    snackbar.value = true;
-    isRecording.value = false;
-    recBtn.value = "Record";
-  };
+  if (Recognition) {
+    speech = new Recognition();
+    speech.continuous = true;
+    speech.interimResults = true;
+    speech.onstart = () => {
+      snackbarText.value = "Recording Started";
+      snackbarColor.value = "success";
+      snackbar.value = true;
+      isRecording.value = true;
+      recBtn.value = "Stop";
+    };
+    speech.onend = () => {
+      snackbarText.value = "Recording Stopped";
+      snackbarColor.value = "error";
+      snackbar.value = true;
+      isRecording.value = false;
+      recBtn.value = "Record";
+    };
 
-  speech.onresult = (evt) => {
-    const script = Array.from(evt.results)
-      .map((result) => result[0])
-      .map((result) => result.transcript)
-      .join(" ");
-    transcript.value = script;
-  };
-});
+    speech.onresult = (evt) => {
+      const script = Array.from(evt.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join(" ");
+      transcript.value = script;
+    };
+  } else {
+    // eslint-disable-next-line no-console
+    console.warn("Speech Recognition is not available in this browser.");
+  }
+};
+
+onMounted(initSpeechRecognition);
 
 const handleRecording = () => {
   if (speech) {
